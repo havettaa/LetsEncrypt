96e214b8c17fa0d5a3a2259a46cdc5646cfd9a90
havettaa <Administrator@FORES023.fores.local>
2020-07-20 01:55:14 +0200

    .net core service

---
+++ Program.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Enrichers;
+using Serilog.Events;
+using Serilog.Sinks;
+using Serilog.Sinks.SystemConsole;
+using Serilog.Sinks.File;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace worker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string loggerTemplate = @"{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u4}]<{ThreadId}> [{SourceContext:l}] {Message:lj}{NewLine}{Exception}";
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var logfile = Path.Combine(baseDir, "App_Data", "logs", "log.txt");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.With(new ThreadIdEnricher())
+                .Enrich.FromLogContext()
+                .WriteTo.Console(LogEventLevel.Information, loggerTemplate)
+                .WriteTo.File(logfile, LogEventLevel.Information, loggerTemplate,
+                    rollingInterval: RollingInterval.Day, retainedFileCountLimit: 90)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("====================================================================");
+                Log.Information($"Application Starts. Version: {System.Reflection.Assembly.GetEntryAssembly()?.GetName().Version}");
+                Log.Information($"Application Directory: {AppDomain.CurrentDomain.BaseDirectory}");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Application terminated unexpectedly");
+            }
+            finally
+            {
+                Log.Information("====================================================================\r\n");
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    // Configure the app here.
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                    services.Configure<AppSettings>(hostContext.Configuration.GetSection("AppSettings"));
+                })
+                .UseSerilog();
+
+    }
+}
